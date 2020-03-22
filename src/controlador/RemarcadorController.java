e9bc821e526731989df5785c27d36c586dbca471
Jorge <jorge.silva8686@gmail.com>
2020-03-22 17:28:53 -0300

    Estructura Remarcador creada
    Lista remarcadores

--- src/controlador/RemarcadorController.java
+++ src/controlador/RemarcadorController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import modelo.Conexion;
 
-public class EmpalmeController extends HttpServlet {
+public class RemarcadorController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,18 +20,15 @@ public class EmpalmeController extends HttpServlet {
         response.setContentType("text/html; charset=UTF-8");
         JSONObject entrada = new JSONObject(request.getParameter("datos"));
         switch (entrada.getString("tipo")) {
-            case "get-empalmes":
+            case "get-remarcadores":
                 out.print(getEmpalmes());
-                break;
-            case "get-select-empalme":
-                out.print(getSelectEmpalme());
                 break;
         }
     }
 
     private JSONObject getEmpalmes() {
         JSONObject salida = new JSONObject();
-        String query = "CALL SP_GET_EMPALMES()";
+        String query = "CALL SP_GET_REMARCADORES()";
         Conexion c = new Conexion();
         c.abrir();
         ResultSet rs = c.ejecutarQuery(query);
@@ -39,8 +36,8 @@ public class EmpalmeController extends HttpServlet {
         try {
             while (rs.next()) {
                 filas += "<tr>";
+                filas += "<td><input type='hidden' value='" + rs.getInt("IDREMARCADOR") + "' /><span>" + rs.getString("NUMREMARCADOR") + "</span></td>";
                 filas += "<td><input type='hidden' value='" + rs.getInt("IDEMPALME") + "' /><span>" + rs.getString("NUMEMPALME") + "</span></td>";
-                filas += "<td><input type='hidden' value='" + rs.getInt("IDFASE") + "' /><span>" + rs.getString("NOMFASE") + "</span></td>";
                 filas += "<td><button style='font-size:10px; padding: 0.1 rem 0.1 rem;' type='button' class='btn btn-sm btn-warning' onclick='activarEdicion(this)'>Editar</button></td>";
                 filas += "</tr>";
             }
@@ -53,21 +50,6 @@ public class EmpalmeController extends HttpServlet {
             salida.put("estado", "error");
             salida.put("error", ex);
         }
-        c.cerrar();
-        return salida;
-    }
-    
-    private JSONObject getSelectEmpalme() {
-        JSONObject salida = new JSONObject();
-        String query = "CALL SP_GET_EMPALMES()";
-        Conexion c = new Conexion();
-        c.abrir();
-        ResultSet rs = c.ejecutarQuery(query);
-
-        String options = modelo.Util.armarSelect(rs, "0", "Seleccione", "IDEMPALME", "NUMEMPALME");
-        salida.put("options", options);
-        salida.put("estado", "ok");
-        System.out.println(salida);
         c.cerrar();
         return salida;
     }
---
+++ web/modulos/remarcador.js
@@ -0,0 +1,47 @@
+function getRemarcadores() {
+    var datos = {
+        tipo: 'get-remarcadores'
+    };
+    $.ajax({
+        url: 'RemarcadorController',
+        type: 'post',
+        data: {
+            datos: JSON.stringify(datos)
+        },
+        success: function (resp) {
+            var obj = JSON.parse(resp);
+            if (obj.estado === 'ok') {
+                $('#tabla-remarcadores tbody').html(obj.tabla);
+            }
+        },
+        error: function (a, b, c) {
+            console.log(a);
+            console.log(b);
+            console.log(c);
+        }
+    });
+}
+
+function getSelectEmpalme() {
+    var datos = {
+        tipo: 'get-select-empalme'
+    };
+    $.ajax({
+        url: 'EmpalmeController',
+        type: 'post',
+        data: {
+            datos: JSON.stringify(datos)
+        },
+        success: function (resp) {
+            var obj = JSON.parse(resp);
+            if (obj.estado === 'ok') {
+                $('#select-empalme').html(obj.options);
+            }
+        },
+        error: function (a, b, c) {
+            console.log(a);
+            console.log(b);
+            console.log(c);
+        }
+    });
+}

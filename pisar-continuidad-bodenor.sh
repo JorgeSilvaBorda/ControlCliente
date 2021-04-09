d861acdbd99f813a64b79a5dd6f428526eb4e31b
Jorge Silva <jorge.silva8686@gmail.com>
2021-04-09 17:07:13 -0400

    Procedimiento para pisar continuidad

---
+++ pisar-continuidad-bodenor.sh
@@ -0,0 +1,4 @@
+nohup mysql -u root -pBodenor848 bodenor -e "call SP_OPS_PISAR_CONTINUIDAD_CIRCUTOR('2021-03-28')" &
+nohup mysql -u root -pBodenor848 bodenor -e "call SP_OPS_PISAR_CONTINUIDAD_PM710('2021-03-28')" &
+nohup mysql -u root -pBodenor848 bodenor -e "call SP_OPS_PISAR_CONTINUIDAD_PM5300('2021-03-28')" &
+
